ca9a12ce77c036fb26e200e1dd393b89dff2bffe
Siniavskay-Nadejda <87280720+Siniavskay-Nadejda@users.noreply.github.com>
2021-12-16 23:11:26 +0200

    cщхраняет домашнюю работу №8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player'
+import throttle from "lodash.throttle"
+const PLAYER_KEY = "videoplayer-current-time"
+const iframeEl = document.querySelector('iframe');
+ const player = new Player(iframeEl);
 
+populatePlayer()
+const onPlay = function (e) {
+   const seconds = e.seconds 
+   console.log(seconds)  
+    localStorage.setItem(PLAYER_KEY, seconds)
+     
+  };
+function populatePlayer() {
+    const savedPlayer = localStorage.getItem(PLAYER_KEY)
+    if (savedPlayer) {
+        player.setCurrentTime(savedPlayer)
+        
+    }
+     
+};
+player.on('timeupdate',throttle(onPlay,1000))
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from "lodash.throttle"
 
+const formEl = document.querySelector(".feedback-form");
+
+const inputEl = document.querySelector(".feedback-form input");
+
+const textareaEl = document.querySelector(".feedback-form textarea");
+const FORM_KEY = "feedback-form-state"
+const formData = {};
+populateFofm()
+ 
+formEl.addEventListener("submit", onFormSubmit);
+   
+formEl.addEventListener("input", throttle(formDataValue ,500 ))
+ function formDataValue (e) { 
+    formData[e.target.name] = e.target.value
+    console.log(formData)
+    localStorage.setItem(FORM_KEY, JSON.stringify(formData))
+};
+    
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    evt.currentTarget.reset()
+    localStorage.removeItem(FORM_KEY)
+    
+}
+
+function populateFofm() {
+    const sevedForm = localStorage.getItem(FORM_KEY)
+    if (sevedForm) {
+        
+        textareaEl.value = JSON.parse(sevedForm).message;
+        inputEl.value = JSON.parse(sevedForm).email;
+         const parsedform = JSON.parse(sevedForm)
+    console.log(parsedform)
+    };
+   
+    
+};
